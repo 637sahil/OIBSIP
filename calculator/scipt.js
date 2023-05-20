4894201b6bdc5eeb511d157c364b1f12fb25cdab
637sahil <86182892+637sahil@users.noreply.github.com>
2023-05-20 12:17:47 +0530

    FIRST COMMIT
    oasis infobyte tasks

---
+++ calculator/scipt.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll("span");
+const value = document.getElementById("value");
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    try {
+      if (this.innerText === "C") {
+        value.innerText = "";
+      } else if (this.innerText === "=") {
+        value.innerText = eval(value.innerText);
+      } else {
+        value.innerText += this.innerText;
+      }
+    } catch (error) {
+      value.innerText = "Invalid";
+    }
+  });
+});
